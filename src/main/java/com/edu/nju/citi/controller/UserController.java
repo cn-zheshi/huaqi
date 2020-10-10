c0368f47ad6b098062160df6d33d2057f73ac914
潘昱光 <1760864645@qq.com>
2020-10-10 09:21:08 +0800

    新增service

--- src/main/java/com/edu/nju/citi/controller/UserController.java
+++ src/main/java/com/edu/nju/citi/controller/UserController.java
@@ -3,6 +3,7 @@ package com.edu.nju.citi.controller;
 
 import com.edu.nju.citi.VO.ResponseVO;
 import com.edu.nju.citi.form.*;
+import com.edu.nju.citi.service.UserService;
 import com.form.UserForm;
 import com.form.UserLoginForm;
 import com.sun.xml.messaging.saaj.packaging.mime.internet.ContentType;
@@ -17,6 +18,7 @@ import java.util.UUID;
 @RestController
 @RequestMapping("/api/v1/account") //domain为网站的域名
 public class UserController {
+    UserService userService;
     //cookie 用来判断用户是否登陆，但采用什么类存疑
     //账户注册
     @PostMapping("/create")
@@ -24,11 +26,12 @@ public class UserController {
         return null;
     }
 
-    //账户验证
+    /*账户验证 unused
     @GetMapping("/request_verification")
     public ResponseEntity<ResponseVO> verification(Cookie cookie, UUID uuid) {
         return null;
     }
+    */
 
     //验证验证码是否正确
     @PostMapping("/verify")
@@ -48,12 +51,13 @@ public class UserController {
         return null;
     }
 
-
+    /*
+    更改账户信息 unused
     @PostMapping("/info")
     public ResponseEntity<ResponseVO> changeAccountInfo(@RequestHeader("uuid") String uuid, @CookieValue("session_id") String sessionID, @RequestHeader("Content-Type") String type) {
         return null;
     }
-
+    */
 
     @PostMapping("/bind")
     public ResponseEntity<ResponseVO> bindCitiAccount(@RequestHeader("uuid") String uuid, @CookieValue("session_id") String sessionID, @RequestHeader("Content-Type") String type) {
--- src/main/java/com/edu/nju/citi/service/UserService.java
+++ src/main/java/com/edu/nju/citi/service/UserService.java
@@ -1,5 +1,10 @@
 package com.edu.nju.citi.service;
 
+import com.edu.nju.citi.VO.*;
+import com.form.*;
 
 public interface UserService {
+    ResponseVO creat(UserForm usr);
+    ResponseVO login(UserLoginForm loginform);
+    ResponseVO info(String uid);
 }
